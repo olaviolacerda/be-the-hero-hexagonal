7fd732a3020a01a057c96fb9cc47f1461ee3da8d
Olavio Lacerda <olavio.lacerda@hotmail.com>
2020-04-21 16:53:17 -0300

    :bug: improve postOng schema validator

--- src/interface/http/schemas/ong.ts
+++ src/interface/http/schemas/ong.ts
@@ -3,9 +3,9 @@ import joi from '@hapi/joi';
 export const postOng = joi.object({
   body: joi.object({
     name: joi.string().required(),
-    email: joi.string().required(),
-    whatsapp: joi.string().required(),
+    email: joi.string().required().email(),
+    whatsapp: joi.number().required().min(10).max(11),
     city: joi.string().required(),
-    uf: joi.string().required(),
+    uf: joi.string().required().length(2),
   }),
 });
